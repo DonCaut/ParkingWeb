310e3413ec4aa40dbe6a32cb6dc077a52ad3b84c
DonCaut <elpolodog@gmail.com>
2024-06-12 18:25:51 -0400

    subiendo el proyecto

---
+++ api/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const { Pool } = require('pg');
+const path = require('path');
+
+
+//conexión a la base de datos
+const pool = new Pool({
+    user: 'postgres',
+    password: 'mere1',
+    host: 'localhost', 
+    database: 'parkingtest',
+    port: 5432, 
+});
+
+// para parsear JSON
+app.use(express.json());
+
+
+//  archivos estáticos para que express los pueda usar
+app.use('/css', express.static(__dirname + '/../css'));
+app.use('/images', express.static(__dirname + '/../images'));
+app.use('/scripts', express.static(__dirname + '/../scripts'));
+
+// HTML principal para que se se abra en localhost
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../main.html'));
+});
+
+// Ruta para manejar el login
+app.post('/login', async (req, res) => {
+    const { phone, password } = req.body;
+    try {
+        const result = await pool.query('SELECT * FROM usuarios WHERE telefono = $1 AND password = $2', [phone, password]);
+        if (result.rows.length > 0) {
+            res.json({ success: true });
+        } else {
+            res.json({ success: false, message: 'Teléfono o contraseña incorrectos' });
+        }
+    } catch (error) {
+        console.error('Error al realizar el login:', error);
+        res.status(500).json({ success: false, message: 'Error al realizar el login' });
+    }
+});
+
+// Ruta para registrar usuario
+app.post('/register', async (req, res) => {
+    const { nombre, phone, password, tipo } = req.body;
+    try {
+        await pool.query(
+            'INSERT INTO usuarios (nombre,telefono,tipo,password) VALUES ($1, $2, $3, $4)',
+            [nombre, phone, tipo, password]
+        );
+        res.json({ success: true });
+    } catch (error) {
+        console.error('Error al registrar usuario:', error);
+        res.status(500).json({ success: false, message: 'Error al registrar usuario' });
+    }
+});
+
+
+
+// para ver la tabla usaurios
+app.get('/usuarios', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM usuarios');
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error al obtener usuarios:', error);
+        res.status(500).json({ error: 'Error al obtener usuarios' });
+    }
+});
+
+
+
+// Más rutas pronto
+
+// Iniciar el servidor
+const puerto = 3000; 
+app.listen(puerto, () => {
+    console.log(`Servidor Express escuchando en el puerto ${puerto}`);
+});
---
+++ scripts/scripts.js
@@ -0,0 +1,93 @@
+function navigateTo(pageId) {
+    // Ocultar los contenidos
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.classList.remove('active'));
+
+    // Mostrar el contenido de la página
+    document.getElementById(pageId).classList.add('active');
+}
+
+// event listeners de los botones de navegación
+document.querySelectorAll('.page-button').forEach(button => {
+    button.addEventListener('click', function() {
+        const targetPageId = this.getAttribute('data-target');
+        navigateTo(targetPageId);
+    });
+});
+                                             //  Función de login
+async function handleLogin(event) { 
+    event.preventDefault(); // Prevenir el envío del formulario
+
+    const phone = document.getElementById('phone').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ phone, password })
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            navigateTo('pageTestLogin'); // Redirigir a la página principal del usuario (por hacer)
+        } else {
+            alert(data.message); // Mostrar mensaje de error si el login falla
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error al intentar iniciar sesión. Por favor, intenta de nuevo.');
+    }
+}
+
+
+let selectedAccountType = '';
+
+// Manejar la selección del tipo de cuenta
+document.querySelectorAll('.page-button').forEach(button => {
+    button.addEventListener('click', function() {
+        const tipoCuenta = this.textContent;
+        sessionStorage.setItem('tipoCuenta', tipoCuenta);
+        const targetPageId = this.getAttribute('data-target');
+        navigateTo(targetPageId);
+    });
+});
+
+// Función para manejar el envío del formulario
+function handleRegister(event) {
+    event.preventDefault();  
+
+    const tipo = sessionStorage.getItem('tipoCuenta');
+    const nombre = document.getElementById('nombre-registro').value;
+    const phone = document.getElementById('phone-registro').value;
+    const password = document.getElementById('password-registro').value;
+
+    fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nombre, phone, password, tipo })
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result.success) {
+            alert('Cuenta registrada con éxito');
+            navigateTo('pageTestRegistro');
+        } else {
+            alert('Error al registrar la cuenta');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error al registrar la cuenta');
+    });
+}
+
+
+
+
+
